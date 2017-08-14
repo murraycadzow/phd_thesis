d14f658e3225c11d71270380a8119dc6e06d2197
murray.cadzow <murray.cadzow@otago.ac.nz>
2017-08-15 06:55:42 +1200

    include supplementary tables

--- preview_thesis.R
+++ preview_thesis.R
@@ -1,2 +1,2 @@
-bookdown::preview_chapter(c('index.Rmd', '01-Introduction.Rmd','02-Methods.Rmd','03-SelectionPipeline.Rmd','04-Clustering.Rmd', '05-Selection_and_association.Rmd','06-Conclusion_discussions.Rmd'))
+bookdown::preview_chapter(c('index.Rmd', '01-Introduction.Rmd','02-Methods.Rmd','03-SelectionPipeline.Rmd','04-Clustering.Rmd', '05-Selection_and_association.Rmd','06-Conclusion_discussions.Rmd','A5-Supplementary_Tables.Rmd'))
 
