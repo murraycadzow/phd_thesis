6ff702af127701869f8563fd4e1aa37e86f99b61
murray.cadzow <murray.cadzow@otago.ac.nz>
2018-06-24 20:54:45 +1200

    add % to table
    closes #66

--- scripts/methods_tables.R
+++ scripts/methods_tables.R
@@ -27,19 +27,19 @@ ce_clinical_table <- function() {
     filter(!is.na(GOUT), !is.na(SEX), ETHCLASS %in% c(1,2,3)) %>% # should I ensure Gout and sex are complete?
     group_by(ETHCLASS,GOUT) %>% 
     summarise(Age = paste0(round(mean(AGECOL, na.rm=TRUE),2)," (", round(sd(AGECOL, na.rm=TRUE),2),")"),
-              Sex = round(sum(SEX ==1, na.rm = TRUE)/ sum(SEX %in% c(1,2), na.rm =TRUE) * 100,2),
+              Sex = paste0(sum(SEX ==1, na.rm = TRUE), " (",round(sum(SEX ==1, na.rm = TRUE)/ sum(SEX %in% c(1,2), na.rm =TRUE) * 100,2),")"),
               BMI = paste0(round(mean(BMI, na.rm=TRUE),2), " (", round(sd(BMI, na.rm = TRUE),2),")"),
               Waist = paste0(round(mean(WAIST, na.rm=TRUE),2), " (", round(sd(WAIST, na.rm=TRUE),2), ")"),
-              n_diabetes = sum(DIABETES == 2, na.rm=TRUE),
+              n_diabetes = paste0(sum(DIABETES == 2, na.rm=TRUE)," (",round(sum(DIABETES == 2, na.rm=TRUE)/NROW(GOUT)*100,2),")"),
               n_gout= NROW(GOUT),
-              n_kidney = sum(KIDNEY==2, na.rm=TRUE),
-              n_heart = sum(HEART == 2, na.rm = TRUE),
-              n_Fatty_liver = sum(FATTYLIVER, na.rm=TRUE),
+              n_kidney = paste0(sum(KIDNEY==2, na.rm=TRUE)," (", round(sum(KIDNEY==2, na.rm=TRUE)/NROW(GOUT)*100, 2),")"),
+              n_heart = paste0(sum(HEART == 2, na.rm = TRUE), " (",round(sum(HEART == 2, na.rm = TRUE)/NROW(GOUT) * 100,2),")"),
+              n_Fatty_liver = paste0(sum(FATTYLIVER==2, na.rm=TRUE)," (",round(sum(FATTYLIVER == 2, na.rm = TRUE)/NROW(GOUT)*100,2),")"),
               #percentMale = signif(sum(SEX == 1)/NROW(SEX),digits = 2) * 100,
               meanSystolic = paste0(round(mean(SYSTOLIC,na.rm=TRUE), 1), " (",round(sd(SYSTOLIC,na.rm=TRUE), 1) ,")"),
               meanDiastolic = paste0(round(mean(DIASTOLIC, na.rm=TRUE),1), " (", round(sd(DIASTOLIC,na.rm=TRUE), 1),")")
               ) %>% ungroup() %>% 
-    mutate(GOUT = ifelse(GOUT == 2, "Gout", "Control"), ETHCLASS = unlist(lapply(ETHCLASS, function(x){switch(x, "1"={return("EP")}, "2" ={return("WP")}, "3" = {return("CAU")})}))) %>% unite(category, c("ETHCLASS","GOUT"), sep = '_') %>% gather("var","value", -category) %>% spread(category, value) %>% mutate(var = c('Mean age (SD)', "Mean BMI (SD)", "Mean Systolic BP (SD)", "Mean Diastolic BP (SD)","Diabetes (n)", "Fatty liver (n)", "n", "Heart disease (n)", "Kidney disease (n)", "Sex (% male)", "Mean Waist cm (SD)"))
+    mutate(GOUT = ifelse(GOUT == 2, "Gout", "Control"), ETHCLASS = unlist(lapply(ETHCLASS, function(x){switch(x, "1"={return("EP")}, "2" ={return("WP")}, "3" = {return("CAU")})}))) %>% unite(category, c("ETHCLASS","GOUT"), sep = '_') %>% gather("var","value", -category) %>% spread(category, value) %>% mutate(var = c('Mean age, years (SD)', "Mean BMI (SD)", "Mean Systolic BP (SD)", "Mean Diastolic BP (SD)","Diabetes, n (%)", "Fatty liver, n (%)", "n", "Heart disease, n (%)", "Kidney disease, n (%)", "Sex, n male (% male)", "Mean Waist cm (SD)"))
  return(tab[c(6,9,4,5,7,8,1:3,10),])
 }
 
